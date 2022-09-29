0177398b7929d3800d85cf08f09fded74f6f00be
Masih@PCM <masihalig@gmail.com>
2022-09-29 21:18:34 +0530

    add Swap_Two_Numbers

---
+++ Swap_Two_Numbers.js
@@ -0,0 +1,13 @@
+function swap(a, b){
+    //write the code to swap two numbers inside this block
+    let num;
+    num = a;
+    a = b;
+    b = num;
+    
+    
+    //dont change below code
+    console.log('a value is =', a);
+    console.log('b value is =', b)
+}
+swap(65, 13);
