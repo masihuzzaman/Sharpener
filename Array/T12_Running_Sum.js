1e8aaee684b25d6b20c252561c640abeb8a18941
Masih <masihalig@gmail.com>
2022-10-10 12:30:49 +0530

    update T12_Running_Sum.js

--- Array/T12_Running_Sum.js
+++ Array/T12_Running_Sum.js
@@ -19,5 +19,8 @@ var runningSum = function (nums) {
     for (let i = 0; i < nums.length - 1; i++) {
         arr[0] = nums[0];
         arr[i + 1] = arr[i] + nums[i + 1];
-    } return arr;
+    } 
+    //return arr;
+    console.log(arr);
 }
+runningSum([1,2,3,4,5]);
