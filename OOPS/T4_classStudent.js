1c6a16643c8ea7ca97527286eba4a7799ab06ffb
Masih <masihalig@gmail.com>
2022-10-26 17:47:24 +0530

    Update T4_classStudent.js

--- OOPS/T4_classStudent.js
+++ OOPS/T4_classStudent.js
@@ -1,3 +1,5 @@
+/* Create a class named 'Student' with String variable 'name' and integer variable 'roll_no'. Use Constructors to Assign the value of roll_no as '2' and that of name as "John" by creating an object of the class Student. */
+
 class Student {
     name = "";
     constructor(name, roll_no) {
