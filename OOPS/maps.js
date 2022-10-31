9f5c8b408c7ca96e9898fd5ec0314e4d2c36dfb9
Masih <masihalig@gmail.com>
2022-10-31 07:33:53 +0530

    Create maps.js

---
+++ OOPS/maps.js
@@ -0,0 +1,10 @@
+//maps are not objects
+var myMap = new Map();
+myMap.set(1, "One");
+myMap.set(2, "Two");
+myMap.set(3, "Three");
+myMap.set(4, "Four");
+myMap.set(5, "Five");
+
+console.log(myMap);
+
