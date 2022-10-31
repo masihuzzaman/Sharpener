fedf6125fc9654fb2ea0281708c4218dd71b8498
Masih <masihalig@gmail.com>
2022-10-31 09:07:21 +0530

    Update maps.js

--- OOPS/maps.js
+++ OOPS/maps.js
@@ -1,10 +1,18 @@
+//Objects are used for storing keyed collections.
+//Arrays are used for storing ordered collections.
+//Map is a collection of keyed data items, just like an Object.
+//But the main difference is that Map allows keys of any type.
 //maps are not objects
-var myMap = new Map();
-myMap.set(1, "One");
-myMap.set(2, "Two");
-myMap.set(3, "Three");
-myMap.set(4, "Four");
-myMap.set(5, "Five");
+let map = new Map();
 
-console.log(myMap);
+map.set('1', 'str1');   // a string key
+map.set(1, 'num1');     // a numeric key
+map.set(true, 'bool1'); // a boolean key
+
+// remember the regular Object? it would convert keys to string
+// Map keeps the type, so these two are different:
+console.log(map.get(1)); // 'num1'
+console.log(map.get('1')); // 'str1'
+console.log(map.get(true)); // 'bool1'
+console.log(map.size); // 3
 
