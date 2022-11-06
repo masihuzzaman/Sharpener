ebb0855fc053c27dbe34970dc601f17843589bde
Masih <masihalig@gmail.com>
2022-11-06 12:25:47 +0530

    Update T9_count_objects.js

--- OOPS/T9_count_objects.js
+++ OOPS/T9_count_objects.js
@@ -1,14 +1,16 @@
 /* Write a program to create a class count_objects and calculate the number of objects that has been created in realtime. */
 
-class Count_objects {
-    static count = 0;
-    countObj() {
+/* In JavaScript, functions are first-class objects, because they can have properties and methods just like any other object. What distinguishes them from other objects is that functions can be called. In brief, they are Function objects */
+
+     function Count_objects() {
         Count_objects.count++;
     }
-}
-//Object.count = 0;
+
+
+Count_objects.count = 0;
 
 let obj1 = new Count_objects;
 let obj2 = new Count_objects;
 let obj3 = new Count_objects;
+let obj4 = new Count_objects;
 console.log(Count_objects.count);
