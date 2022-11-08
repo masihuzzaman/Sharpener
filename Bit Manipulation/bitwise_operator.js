7d51747cf48cf2ead2d83e0b9e74a4a9f9fb0ea3
Masih <masihalig@gmail.com>
2022-11-08 23:07:27 +0530

    Update bitwise_operator.js

--- Bit Manipulation/bitwise_operator.js
+++ Bit Manipulation/bitwise_operator.js
@@ -45,6 +45,8 @@ console.log(5 << 1);
 //  JavaScript (Sign Preserving) Bitwise Right Shift (>>)
 //============================
 //  This is a sign preserving right shift. Copies of the leftmost bit are pushed in from the left, and the rightmost bits fall off:
+//  Bits shifted off to the right are discarded. Bit positions that free up on the left are filled with copies of the bit that was previously leftmost.
+//Because the leftmost bit defines the sign of the number, the resulting sign never changes, which explains "sign-propagating" in the operator's name.
 
 //      5 = 0000 0101
 //   5>>1 = 0000 0010   (2)
@@ -59,6 +61,15 @@ console.log(-5 >> 1);
 //  JavaScript (Zero Fill) Right Shift (>>>)
 //===========================
 //  This is a zero fill right shift. One or more zero bits are pushed in from the left, and the rightmost bits fall off:
+//  Similar to the previous operator, Zero-fill right shift (>>>) shifts bits of the first operand to the right by the number of positions defined by the second operand. However, vacant bit positions on the left are filled with zeros. This has two implications:
+
+/* The result will always be positive, because a zero in the leftmost bit means a positive number.
+For positive numbers, both right shift operators, >> and >>>, always return the same result.
+For (a somewhat wild) example, -9 >>> 2 returns... 1073741821:
+
+SNIPPET
+-11111111111111111111111111110111
++00111111111111111111111111111101 */
 
 //      5 = 0000 0101
 //  5>>>1 = 0000 0010   (2)
