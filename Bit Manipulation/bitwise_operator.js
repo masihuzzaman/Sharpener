3f402be0d31113206c302df3fcb54440a8ba569b
Masih <masihalig@gmail.com>
2022-11-08 20:58:54 +0530

    Create bitwise_operator.js

---
+++ Bit Manipulation/bitwise_operator.js
@@ -0,0 +1,65 @@
+//=============================
+//  JavaScript Bitwise AND (&)
+//=============================
+//  5 = 0101
+//  5 = 0001
+//  &--------
+//      0001 = 1
+console.log(5 & 1);
+
+//============================
+//  JavaScript Bitwise XOR (^)
+//============================
+//  5 = 0101
+//  5 = 0001
+//  ^--------
+//      0100 = 4
+console.log(5 ^ 1);
+
+//============================
+//  JavaScript Bitwise OR (|)
+//============================
+//  5 = 0101
+//  5 = 0001
+//  |--------
+//      0101 = 5
+console.log(5 | 1);
+
+//============================
+//  JavaScript Bitwise NOT (~)
+//============================
+//  5 = 0000 0101
+// ~5 = 1111 1010 (-6)
+console.log(~5);
+
+//============================
+//  JavaScript (Zero Fill) Bitwise Left Shift (<<)
+//============================
+//  This is a zero fill left shift. One or more zero bits are pushed in from the right, and the leftmost bits fall off:
+
+//      5 = 0000 0101
+//   5<<1 = 0000 1010   (10)
+console.log(5 << 1);
+
+//============================
+//  JavaScript (Sign Preserving) Bitwise Right Shift (>>)
+//============================
+//  This is a sign preserving right shift. Copies of the leftmost bit are pushed in from the left, and the rightmost bits fall off:
+
+//      5 = 0000 0101
+//   5>>1 = 0000 0010   (2)
+console.log(5 >> 1);
+
+//     -5 = 1111 1011
+//  -5>>1 = 1111 1101   (-3)
+console.log(-5 >> 1);
+
+
+//===========================
+//  JavaScript (Zero Fill) Right Shift (>>>)
+//===========================
+//  This is a zero fill right shift. One or more zero bits are pushed in from the left, and the rightmost bits fall off:
+
+//      5 = 0000 0101
+//  5>>>1 = 0000 0010   (2)
+console.log(5 >>> 1);
