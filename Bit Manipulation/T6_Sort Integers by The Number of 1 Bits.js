442264d7f7c77a7af058e9129b46d0d1513b89ba
Masih <masihalig@gmail.com>
2022-11-11 09:39:15 +0530

    Update T6_Sort Integers by The Number of 1 Bits.js

--- Bit Manipulation/T6_Sort Integers by The Number of 1 Bits.js
+++ Bit Manipulation/T6_Sort Integers by The Number of 1 Bits.js
@@ -20,6 +20,51 @@ Explantion: All integers have 1 bit in the binary representation, you should jus
  * @param {number[]} arr
  * @return {number[]}
  */
+
+/*  By default, the sort() function sorts values as strings.
+
+ This works well for strings ("Apple" comes before "Banana").
+ 
+ However, if numbers are sorted as strings, "25" is bigger than "100", because "2" is bigger than "1".
+ 
+ Because of this, the sort() method will produce incorrect result when sorting numbers.
+ 
+ You can fix this by providing a compare function: 
+ 
+ Example
+ const points = [40, 100, 1, 5, 25, 10];
+ points.sort(function(a, b){return a - b});
+
+ Use the same trick to sort an array descending:
+
+ Example
+ const points = [40, 100, 1, 5, 25, 10];
+ points.sort(function(a, b){return b - a});
+
+ The Compare Function
+The purpose of the compare function is to define an alternative sort order.
+
+The compare function should return a negative, zero, or positive value, depending on the arguments:
+
+function(a, b){return a - b}
+When the sort() function compares two values, it sends the values to the compare function, and sorts the values according to the returned (negative, zero, positive) value.
+
+If the result is negative, a is sorted before b.
+
+If the result is positive, b is sorted before a.
+
+If the result is 0, no changes are done with the sort order of the two values.
+
+Example:
+
+The compare function compares all the values in the array, two values at a time (a, b).
+
+When comparing 40 and 100, the sort() method calls the compare function(40, 100).
+
+The function calculates 40 - 100 (a - b), and since the result is negative (-60),  the sort function will sort 40 as a value lower than 100.
+*/
+
+
 var sortByBits = function (arr) {
     let sortedArray = arr.sort((a, b) => bitCount(a) - bitCount(b) || a - b);
     console.log(sortedArray);
@@ -30,11 +75,11 @@ const bitCount = num => {
         sum += num & 1;
         num = num >> 1;
     }
-    console.log(sum);
+    //console.log(sum);
     return sum;
 };
 
-//let arr = [0,1,2,3,4,5,6,7,8]
-//sortByBits(arr)
-bitCount(5)
+let arr = [0,1,2,3,4,5,6,7,8]
+sortByBits(arr)
+//bitCount(5)
  
