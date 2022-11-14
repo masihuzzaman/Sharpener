27a3958b373d56d8d2e6f8996b8c0f10b5baad84
Masih <masihalig@gmail.com>
2022-11-14 22:56:48 +0530

    Update T9_count_objects

--- OOPS/T9_count_objects.js
+++ OOPS/T9_count_objects.js
@@ -2,7 +2,7 @@
 
 /* In JavaScript, functions are first-class objects, because they can have properties and methods just like any other object. What distinguishes them from other objects is that functions can be called. In brief, they are Function objects */
 
-     function Count_objects() {
+/*      function Count_objects() {
         Count_objects.count++;
     }
 
@@ -13,4 +13,18 @@ let obj1 = new Count_objects;
 let obj2 = new Count_objects;
 let obj3 = new Count_objects;
 let obj4 = new Count_objects;
+console.log(Count_objects.count);
+ */
+
+class Count_objects {
+    static count = 0;
+    constructor() {
+        Count_objects.count += 1;
+    }
+}
+
+let object_1 = new Count_objects();
+let object_2 = new Count_objects();
+let object_3 = new Count_objects();
+
 console.log(Count_objects.count);
