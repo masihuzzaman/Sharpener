f4e55386805ef51f6b1f90a6c8d43139d5aeddc5
Masih <masihalig@gmail.com>
2022-11-17 22:54:12 +0530

    Update maps.js

--- Maps and Sets/maps.js
+++ Maps and Sets/maps.js
@@ -1,4 +1,4 @@
-//Objects are used for storing keyed collections.
+/* //Objects are used for storing keyed collections.
 //Arrays are used for storing ordered collections.
 //Map is a collection of keyed data items, just like an Object.
 //But the main difference is that Map allows keys of any type.
@@ -15,4 +15,79 @@ console.log(map.get(1)); // 'num1'
 console.log(map.get('1')); // 'str1'
 console.log(map.get(true)); // 'bool1'
 console.log(map.size); // 3
+ */
+//-----------------------------------------------------------------
+
+/* let map = new Map();
+map.set('1', 'str1');
+map.set("1", "str2");
+map.set(1, 'num1');
+map.set(true, 'bool1');
+
+console.log(map.get(1));
+console.log(map.get('1'));
+console.log(map.get(true)); */
+
+/* map[2] = 'num2';
+console.log(map.get(2)); */
+
+// create an object and store it as a key in a map
+let john = {
+    name: "John",
+    age: 33
+};
+
+let map = new Map();
+map.set(john, "Abraham");
+console.log(map.get(john));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
