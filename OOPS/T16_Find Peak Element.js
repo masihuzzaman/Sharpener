674e7d132915d805cc084b9125171d1705263418
Masih <masihalig@gmail.com>
2022-11-23 00:31:55 +0530

    Update T16_Find Peak Element.js

--- OOPS/T16_Find Peak Element.js
+++ OOPS/T16_Find Peak Element.js
@@ -6,15 +6,12 @@ You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is al
 
 You must write an algorithm that runs in O(log n) time.
 
- 
-
 Example 1:
-
 Input: nums = [1,2,3,1]
 Output: 2
 Explanation: 3 is a peak element and your function should return the index number 2.
-Example 2:
 
+Example 2:
 Input: nums = [1,2,1,3,5,6,4]
 Output: 5
 Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6. */
@@ -32,16 +29,5 @@ var findPeakElement = function (nums) {
     }
     return left;
 };
-
-// Linear Search
-// Time: O(N)
-/* var findPeakElement = function(nums) {
-    let left = 0, right = nums.length-1, mid;
-    
-    while(left < right) {
-        mid = Math.floor((right+left)/2);
-        if(nums[mid] > nums[mid+1]) right = mid;
-        else left = mid+1;
-    }
-    return left;
-}; */
+console.log(findPeakElement([1,2,1,3,5,6,4]));
+console.log(findPeakElement([1,2,3,1]));
