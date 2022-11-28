5238baeb52f8cb2b5f2a21db3248589adc02d980
Masih <masihalig@gmail.com>
2022-11-28 22:34:06 +0530

    Create 04_Valid_Parenthesis.js

---
+++ Stacks and Ques/04_Valid_Parenthesis.js
@@ -0,0 +1,46 @@
+/* Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false */
+
+var isValid = function(s) {
+    const stack = [];
+    const map = {
+      '(': ')',
+      '[': ']',
+      '{': '}'
+    }
+
+    for (let i = 0 ; i < s.length ; i++) {
+        let c = s[i];
+        if (map[c]) {
+          stack.push(map[c])
+        } else if (c !== stack.pop()) {
+          //console.log(false);
+          return false;
+        }
+    }
+    //console.log(!stack.length);
+    return !stack.length;
+};
+
+// isValid("(({}))");
+// isValid("(}))");
+
