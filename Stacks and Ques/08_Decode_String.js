727ce7c028e8f88a9122e2570d919ab9c0be9911
Masih <masihalig@gmail.com>
2022-12-04 22:00:15 +0530

    Update 08_Decode_String.js

--- Stacks and Ques/08_Decode_String.js
+++ Stacks and Ques/08_Decode_String.js
@@ -1,5 +1,5 @@
-/* Traversal the string s and push into a stack for non-] character
-When we meets the ] character, we should do these steps
+/* Traverse the string s and push into a stack for non-] character.
+When there is a ] character, we should do these steps
 pop all characters until meets [
 pop all numbers to get the repeat count
 repeat the substring and push it back to stack
@@ -9,31 +9,39 @@ const decodeString = s => {
     const stack = [];
     for (const char of s) {
         if (char !== "]") {
-            stack.push(char);
+            stack.push(char); //2[acd
+            console.log(stack);
             continue;
         }
         //
-        let cur = stack.pop();
+        let cur = stack.pop(); // d // 2[ac
         let str = '';
         while (cur !== '[') {
-            str = cur + str;
-            cur = stack.pop();
+            str = cur + str; // d 
+            console.log(str);
+            cur = stack.pop(); // c // 2[a
+            console.log(cur);
+            console.log(stack);
         }
         // getting the character through pop()
         // changing it to a number and
         // checking if its really a number and if 
         // its really a number then we dont want a number
         let num = '';
-        cur = stack.pop();
+        cur = stack.pop(); // a // 2[
         while (!Number.isNaN(Number(cur))) {
             num = cur + num;
+            console.log(num);
             cur = stack.pop();
+            console.log(cur);
         }
 
-        stack.push(cur);
-        stack.push(str.repeat(Number(num)));
+        stack.push(cur); // a // 2[a
+        console.log(stack);
+        stack.push(str.repeat(Number(num))); // d
+        console.log(stack);
     }
     return stack.join('');
 };
 
-console.log(decodeString("2[ac]"));
+console.log(decodeString("2[acd]"));
