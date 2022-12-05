77efb28a9e242b6c8ea452470e2de1ac86c94fea
Masih <masihalig@gmail.com>
2022-12-05 22:23:36 +0530

    Update 08_Decode_String.js

--- Stacks and Ques/08_Decode_String.js
+++ Stacks and Ques/08_Decode_String.js
@@ -1,3 +1,23 @@
+/* Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+The test cases are generated so that the length of the output will never exceed 105.
+
+Example 1:
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+
+Example 2:
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+
+Example 3:
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef" */
+
 /* Traverse the string s and push into a stack for non-] character.
 When there is a ] character, we should do these steps
 pop all characters until meets [
@@ -6,29 +26,24 @@ repeat the substring and push it back to stack
 Finally, we join all the pieces in the stack */
 
 const decodeString = s => {
-    const stack = [];
-    for (const char of s) {
+    let stack = [];
+    for (let char of s) {
         if (char !== "]") {
-            stack.push(char); //2[acd
+            stack.push(char);
             console.log(stack);
             continue;
         }
-        //
-        let cur = stack.pop(); // d // 2[ac
+        let cur = stack.pop();
         let str = '';
         while (cur !== '[') {
-            str = cur + str; // d 
+            str = cur + str;
             console.log(str);
-            cur = stack.pop(); // c // 2[a
+            cur = stack.pop();
             console.log(cur);
             console.log(stack);
         }
-        // getting the character through pop()
-        // changing it to a number and
-        // checking if its really a number and if 
-        // its really a number then we dont want a number
         let num = '';
-        cur = stack.pop(); // a // 2[
+        cur = stack.pop();
         while (!Number.isNaN(Number(cur))) {
             num = cur + num;
             console.log(num);
@@ -36,12 +51,14 @@ const decodeString = s => {
             console.log(cur);
         }
 
-        stack.push(cur); // a // 2[a
+        if (cur) {
+            stack.push(cur);
+        }
         console.log(stack);
-        stack.push(str.repeat(Number(num))); // d
+        stack.push(str.repeat(Number(num)));
         console.log(stack);
     }
     return stack.join('');
 };
 
-console.log(decodeString("2[acd]"));
+console.log(decodeString("2[2[acd]]"));
