cb65a81ec1025487ff776c32429bb5f868b61cb5
Masih <masihalig@gmail.com>
2023-01-02 18:06:56 +0000

    Create Sliding Window Maximum.js

---
+++ Stacks and Ques/19_Sliding Window Maximum.js
@@ -0,0 +1,26 @@
+var maxSlidingWindow = function (nums, k) {
+    const result = [];
+    if (nums.length == 0) return result;
+    const deque = [];
+    const add = n => {
+        while (deque.length > 0 && n > deque[deque.length - 1]) {
+            deque.pop();
+        }
+        deque.push(n);
+    }
+    const remove = n => {
+        if (n == deque[0]) {
+            deque.shift();
+        }
+    }
+    let j = 0;
+    nums.forEach((n, index) => {
+        add(n);
+        if (index >= (k - 1)) {
+            result.push(deque[0]);
+            remove(nums[j]);
+            j++;
+        }
+    });
+    return result;
+};
