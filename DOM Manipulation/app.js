99375684ddf3cf5c9875a0aa4ca44e296b190d86
Masih <masihalig@gmail.com>
2023-02-28 23:06:43 +0530

    Task_03_final
    Learned DOM, the elements and attributes, getElementById selector and 3 properties -- innerText, textContent, innerHTML, and style.borderBottom

--- DOM Manipulation/app.js
+++ DOM Manipulation/app.js
@@ -1,1 +1,19 @@
-// git practice here
+// DOM Manipulation 
+
+// console.dir(document.forms);
+document.title = 'Sharpener Play Ground : DOM'
+
+//GETELEMENTBYID
+// console.log(document.getElementById('header-title')); 
+var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+var header = document.getElementById('main-header');
+// console.log(header);
+console.log(headerTitle.textContent);
+console.log(headerTitle.innerText);
+console.log(headerTitle.innerHTML);
+headerTitle.innerText = 'Masih';
+headerTitle.textContent = 'Zaman';
+headerTitle.innerHTML = '<h3>Sharpener</h3>';
+// headerTitle.style.borderBottom = 'solid 3px #000';
+header.style.borderBottom = 'solid 5px red';
