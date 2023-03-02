520827db719f128e361e929031af3478693ed880
Masih <masihalig@gmail.com>
2023-03-03 01:11:09 +0530

    Task_04_DOM

--- DOM Manipulation/app.js
+++ DOM Manipulation/app.js
@@ -5,15 +5,37 @@ document.title = 'Sharpener Play Ground : DOM'
 
 //GETELEMENTBYID
 // console.log(document.getElementById('header-title')); 
-var headerTitle = document.getElementById('header-title');
+// var headerTitle = document.getElementById('header-title');
 // console.log(headerTitle);
-var header = document.getElementById('main-header');
+// var header = document.getElementById('main-header');
 // console.log(header);
-console.log(headerTitle.textContent);
-console.log(headerTitle.innerText);
-console.log(headerTitle.innerHTML);
-headerTitle.innerText = 'Masih';
-headerTitle.textContent = 'Zaman';
-headerTitle.innerHTML = '<h3>Sharpener</h3>';
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// console.log(headerTitle.innerHTML);
+// headerTitle.innerText = 'Masih';
+// headerTitle.textContent = 'Zaman';
+// headerTitle.innerHTML = '<h3>Sharpener</h3>';
 // headerTitle.style.borderBottom = 'solid 3px #000';
-header.style.borderBottom = 'solid 5px red';
+// header.style.borderBottom = 'solid 5px red';
+
+//GETELEMENTBYCLASSNAME
+var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
+
+//Gives error
+// items.styel.backgroundColor = 'grey';
+
+// for(var i = 0; i < items.length; i++) {
+//     items[i].style.backgroundColor = 'grey';
+// }
+
+items[2].style.backgroundColor = 'green';
+
+for(var i = 0; i < items.length; i++) {
+    items[i].style.fontWeight = 'bold';
+    items[i].style.color = 'red';   
+}
