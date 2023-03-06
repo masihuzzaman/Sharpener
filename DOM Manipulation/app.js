1e886561fc1ad538fbb578d6f4505338e9554558
Masih <masihalig@gmail.com>
2023-03-06 21:12:45 +0530

    Task_06_DOM_querySelector_answer

--- DOM Manipulation/app.js
+++ DOM Manipulation/app.js
@@ -69,6 +69,7 @@ var odd = document.querySelectorAll('li:nth-child(odd)');
 var even = document.querySelectorAll('li:nth-child(even)');
 
 for(var i = 0; i < odd.length; i++) {
-    odd[i].style.backgroundColor = '#f4f4f4';
-    even[i].style.backgroundColor = '#ccc';
+    odd[i].style.backgroundColor = 'green';
+    even[i].style.color = 'green';
 }
+
