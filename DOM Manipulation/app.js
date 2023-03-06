f01b2caa22a8fd0f40cf140e908b8a951b3080f0
Masih <masihalig@gmail.com>
2023-03-06 21:00:34 +0530

    Task_06_DOM_querySelector
    manipulated the DOM as per instructor

--- DOM Manipulation/app.js
+++ DOM Manipulation/app.js
@@ -35,8 +35,40 @@ var li = document.getElementsByTagName('li');
 
 // li[2].style.backgroundColor = 'grey';
 
-for(var i = 0; i < li.length; i++) {
-    li[i].style.fontWeight = 'bold';
-    li[i].style.color = '#fff';  
-    li[i].style.backgroundColor = 'grey'
+// for(var i = 0; i < li.length; i++) {
+//     li[i].style.fontWeight = 'bold';
+//     li[i].style.color = '#fff';  
+//     li[i].style.backgroundColor = 'grey'
+// }
+
+//QUERYSELECTOR
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 10px #666';
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = 'SEND';
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'green';
+
+//QUERYSELECTORALL
+// var titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
 }
