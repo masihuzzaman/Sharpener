844a0115eadf94a5d68c77ffdd4705e1aba6e2ed
Masih <masihalig@gmail.com>
2023-05-28 21:47:08 +0530

    Update main.js
    Return to same basic code to complete task 13

--- Projects/Appointment_Booking_App/main.js
+++ Projects/Appointment_Booking_App/main.js
@@ -4,52 +4,32 @@ const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
-// Retrieve existing users from local storage or initialize an empty array
-let users = JSON.parse(localStorage.getItem('users')) || [];
-
-// Populate the user list with existing users on page load
-populateUserList();
-
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  if (nameInput.value === '' || emailInput.value === '') {
-    showMessage('Please enter all fields', 'error');
-  } else {
-    const user = {
-      name: nameInput.value,
-      email: emailInput.value,
-    };
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
 
-    users.push(user); // Add the new user to the users array
-    localStorage.setItem('users', JSON.stringify(users)); // Store the updated array in local storage
-    populateUserList(); // Update the user list on the page
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const user = {
+            name: nameInput.value,
+            email: emailInput.value
+        };
 
-    showMessage('User added successfully', 'success');
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${user.name} : ${user.email}`));
 
-    nameInput.value = ''; // Clear fields
-    emailInput.value = '';
-  }
-}
+        // Store User Information as an Object
+        localStorage.setItem(user.email, JSON.stringify(user));
+        console.log(user);
+        userList.appendChild(li);
 
-function populateUserList() {
-  userList.innerHTML = ''; // Clear the user list before populating it again
-
-  users.forEach((user) => {
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(`${user.name} : ${user.email}`));
-    userList.appendChild(li);
-  });
-}
-
-function showMessage(message, className) {
-  msg.textContent = message;
-  msg.className = `msg ${className}`;
-
-  setTimeout(() => {
-    msg.textContent = '';
-    msg.className = 'msg';
-  }, 3000);
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
 }
