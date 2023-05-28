11b15d760bcc938ec0b403f26e6a9b85841d96c6
Masih <masihalig@gmail.com>
2023-05-28 23:36:32 +0530

    delete user task 13
    delete user from html as well as local storage

--- Projects/Appointment_Booking_App/main.js
+++ Projects/Appointment_Booking_App/main.js
@@ -1,35 +1,48 @@
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
 
-myForm.addEventListener('submit', onSubmit);
+myForm.addEventListener("submit", onSubmit);
 
 function onSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if(nameInput.value === '' || emailInput.value === '') {
-        msg.classList.add('error');
-        msg.innerHTML = 'Please enter all fields';
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.classList.add("error");
+    msg.innerHTML = "Please enter all fields";
 
-        setTimeout(() => msg.remove(), 3000);
-    } else {
-        const user = {
-            name: nameInput.value,
-            email: emailInput.value
-        };
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const user = {
+      name: nameInput.value,
+      email: emailInput.value,
+    };
 
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${user.name} : ${user.email}`));
+    // Create del btn element
+    const deletebtn = document.createElement("button");
+    // append text node to deletebtn
+    deletebtn.appendChild(document.createTextNode("Delete"));
+    
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${user.name} : ${user.email}`));
+    li.appendChild(deletebtn);
 
-        // Store User Information as an Object
-        localStorage.setItem(user.email, JSON.stringify(user));
-        console.log(user);
-        userList.appendChild(li);
+    deletebtn.addEventListener("click", function (e) {
+      const li = e.target.parentElement;
+      const email = user.email;
 
-        // Clear fields
-        nameInput.value = '';
-        emailInput.value = '';
-    }
+      userList.removeChild(li);
+      localStorage.removeItem(email);
+    });
+
+    // Store User Information as an Object
+    localStorage.setItem(user.email, JSON.stringify(user));
+    userList.appendChild(li);
+
+    // Clear fields
+    nameInput.value = "";
+    emailInput.value = "";
+  }
 }
